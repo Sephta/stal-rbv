2af6c5fa276099b7a5de708cbbf6a23a27d73645
Seth Tal <Seth.tal255@gmail.com>
2021-08-09 13:22:46 -0700

    Finished basic project boilerplate

--- js/index.js
+++ js/index.js
@@ -1,3 +1,31 @@
-const helloWorld = () => {console.log("Hello World!");};
+/**
+ * Seth Tal
+ * 08/09/2021
+ * Fetching Data from r/Business using public reddit api
+ */
 
-helloWorld();
+const subreddit = `business`;
+const listing   = `new`;
+const limit     = `4`;
+const timeframe = `month`;
+
+const dataEndpoint = `https://www.reddit.com/r/${subreddit}/${listing}.json?limit=${limit}&t=${timeframe}`;
+
+const fetchRedditData = (endpoint) => {
+  fetch(endpoint)
+  .then((response) => response.json())
+  .then((responseObject) => {
+    console.log("Data: ", responseObject);
+
+    let data = responseObject.data;
+
+    data?.children.forEach((child) => {
+      console.log(child);
+    });
+  })
+  .catch((error) => {
+    error && console.log("ERROR: ", error);
+  });
+};
+
+fetchRedditData(dataEndpoint);
